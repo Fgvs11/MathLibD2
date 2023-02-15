0d8afdeaa77d30884fa1acf04e6ffd7a8653c716
Fgvs-PC <fgvizcainos@gmail.com>
2023-02-14 18:26:19 -0600

    Cos

--- MathLib.java
+++ MathLib.java
@@ -38,7 +38,19 @@ public class MathLib{
         return ea;
     }
 
-   
+    public double obtenerGrados(double angulo, int opc){
+        if (opc == 1){
+            return conGR(angulo);
+        }
+        return angulo;
+    }
+
+    public double valorAbsoluto(double a){
+        if(a < 0){
+            return -1 * a;
+        }
+        return a;
+    }
     //Metodos de alexius
     public double raizCuadrada(double numero){
         do {
@@ -58,4 +70,22 @@ public class MathLib{
         return resultadoChar;
     }
      */
+
+    //Coseno
+    public double cos(double angulo, int cs){
+        System.out.printf("%-10.10s | %-30.30s | %-10.10s\n","0","1","-");
+        int n = 1;
+        errorMeta(cs);
+        double x = 1, xi;
+        while(true){
+            xi = x + exponente(-1,n) * exponente(angulo,2*n)/factorial(2*n);
+            Double auxcos = valorAbsoluto((valorAbsoluto(xi) - valorAbsoluto(x))/ valorAbsoluto(xi) * 100);
+            System.out.printf("%-10.10s | %-30.30s | %."+cs+"f%s\n",String.valueOf(n),String.valueOf(xi),auxcos, "%");
+            if(auxcos < es){
+                return xi;
+            }
+            n++;
+            x = xi;
+        }
+    }
 }
